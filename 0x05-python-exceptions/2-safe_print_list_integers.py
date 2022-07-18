547f7b010f08c9b297bafec7b6821263d94b3382
Williams Osei Prempeh <52207181+NANA-PREMPEH@users.noreply.github.com>
2022-07-18 12:13:16 +0000

    Update 2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    y = 0
+    i = 0
     j = 0
-    while y < x and x > 0:
+    while i < x and x > 0:
         try:
-            val = my_list[y]
+            val = my_list[i]
             if isinstance(val, int):
-                print(my_list[y], end='')
+                print(my_list[i], end='')
                 j += 1
-            y += 1
+            i += 1
         except IndexError:
             break
     print("")
