9a856102d36a2e394dcfe2acae98d8af4bcebfb4
Williams Osei Prempeh <52207181+NANA-PREMPEH@users.noreply.github.com>
2022-07-18 12:04:56 +0000

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    y = 0
-    while y < x and x > 0:
+    i = 0
+    while i < x and x > 0:
         try:
             print(my_list[i], end='')
-            y += 1
+            i += 1
         except Exception as e:
             break
     print("")
-    return int(y)
+    return int(i)
