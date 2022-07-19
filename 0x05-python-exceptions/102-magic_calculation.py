b73c2e7385822664a0d9ea2fa1c573a5402d9710
NANA-PREMPEH <oseiprempehwilliams@gmail.com>
2022-07-18 20:35:53 -0700

    magic calculation

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    start = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            else:
+                start += (a ** b) / i
+        except Exception:
+            start = b + a
+            break
+    return (start)
