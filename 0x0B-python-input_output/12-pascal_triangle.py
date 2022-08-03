003a5732f0a2929142b479c9997a6ee8cb1d630a
NANA-PREMPEH <oseiprempehwilliams@gmail.com>
2022-08-02 20:42:30 -0700

    pascal

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Function Declaration"""
+
+
+def pascal_triangle(n):
+    list1 = []
+    list2 = []
+    if n == 1:
+        return [[1]]
+    for r in range(1, n):
+        if len in r:
+            len = [1]
