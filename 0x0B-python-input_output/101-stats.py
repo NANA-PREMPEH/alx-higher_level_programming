c22fad02b384b5a4d1166757a089388c00c45e0e
Williams Osei Prempeh <52207181+NANA-PREMPEH@users.noreply.github.com>
2022-08-03 04:33:50 +0000

    Update 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,39 +1,29 @@
 #!/usr/bin/python3
-"""This documents gather stats from stdin"""
-import sys
+"""Function Declaration"""
 
 
-def print_pretty(size, code_dict):
-    """parse important data"""
-    print("File size: {}".format(size))
-    for key, value in sorted(code_dict.items()):
-        if (value != 0):
-            print("{}: {}".format(key, value))
+import fileinput
+import shlex
 
-if __name__ == '__main__':
-    """init code to print the parsed data"""
-    size = 0
-    code_dict = {
-        "200": 0,
-        "301": 0,
-        "400": 0,
-        "401": 0,
-        "403": 0,
-        "404": 0,
-        "405": 0,
-        "500": 0
-    }
-    try:
-        line_counter = 0
-        for line in sys.stdin:
-            line_counter += 1
-            code = line.split()[7]
-            size += int(line.split()[8])
-            if code in code_dict:
-                code_dict[code] += 1
-            if (line_counter % 10 == 0):
-                print_pretty(size, code_dict)
-        print_pretty(size, code_dict)
-    except KeyboardInterrupt:
-        print_pretty(size, code_dict)
-        raise
+
+i = 0
+b = []
+total_size = 0
+for line in fileinput.input():
+    if i == 10:
+        b.sort()
+        for n in b:
+            total_size += n
+            print(f"b: {b}")
+        print(f"File size: {total_size}")
+        total_size = 0
+        b = []
+        i = 0
+        continue
+    a = shlex.split(line)
+    b += [a[5]]
+#    total_size += inta[6])
+    i += 1
+#    for i in b:
+#        print(f"{i}: {a[6]}")
+    
