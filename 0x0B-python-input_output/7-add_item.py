f96d3c0efb40696e51e985334312985897f922a6
NANA-PREMPEH <oseiprempehwilliams@gmail.com>
2022-08-02 19:58:02 -0700

    add item

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Function declaration"""
+
+
+import json
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+try:
+    arguments = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    arguments = []
+
+for i in range(1, len(sys.argv)):
+    arguments += [sys.argv[i]]
+save_to_json_file(arguments, "add_item.json")
