11ba6d85153d2fbec9a98b8ec8731430bb40d391
Williams Osei Prempeh <52207181+NANA-PREMPEH@users.noreply.github.com>
2022-09-07 03:34:15 +0000

    sql introduction

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,2 @@
+-- displays the average temperature (Fahrenheit) by city ordered by temperature
+SELECT city, AVG(value) AS avg_temp FROM hbtn_0c_0.temperatures GROUP BY city ORDER BY avg_temp DESC;
