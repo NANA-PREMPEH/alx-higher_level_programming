a804da54a05dbfb2464bc0ba5798b073f2be6004
William Osei Prempeh <52207181+NANA-PREMPEH@users.noreply.github.com>
2022-10-08 16:10:36 +0000

    Update 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,40 +1,23 @@
 #!/usr/bin/python3
-"""
-Script that takes in the name of a state as an argument and lists all cities
-of that state, using the database hbtn_0e_4_usa.
-"""
-import MySQLdb as mdb
+'''
+script that takes in the name of a state as an argument and lists all cities
+'''
+import MySQLdb
 import sys
-
-
 if __name__ == "__main__":
-    args = {
-        "host": "localhost",
-        "port": 3306,
-        "user": sys.argv[1],
-        "passwd": sys.argv[2],
-        "db": sys.argv[3],
-        "charset": "utf8"
-        }
-    d_base = sys.argv
-    try:
-        conn = mdb.connect(**args)
-        cur = conn.cursor()
-        cur.execute("SELECT cities.name\
-        FROM cities\
-        INNER JOIN states\
-        ON cities.state_id = states.id\
-        WHERE states.name=%s", (sys.argv[4],))
-        rows = cur.fetchall()
-        if rows:
-            for i in range(0, len(rows)):
-                if i == (len(rows) - 1):
-                    print(rows[i][0])
-                else:
-                    print("{}, ".format(rows[i][0]), end='')
-        else:
-            print()
-    except:
-        print()
+    db = MySQLdb.connect("localhost", sys.argv[1], sys.argv[2], sys.argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT cities.name FROM cities\
+            INNER JOIN states\
+            ON cities.state_id = states.id\
+            WHERE states.name =  %s", [sys.argv[4]])
+
+    rows = cur.fetchall()
+    result = []
+    for row in rows:
+        result.append(row[0])
+
+    print(', '.join(result))
     cur.close()
-    conn.close()
+    db.close()
