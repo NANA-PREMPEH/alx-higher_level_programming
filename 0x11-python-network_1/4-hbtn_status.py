3176221637d98ed953e40e97bf7a1b553e4cd307
William Osei Prempeh <52207181+NANA-PREMPEH@users.noreply.github.com>
2022-10-22 04:18:25 +0000

    hbtn_status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """
-Module using request that fetches https://intranet.hbtn.io/status
+Module using request that fetches https://alx-intranet.hbtn.io/status
 """
 import requests
 
 
 def main():
     """
-    Function that fetches https://intranet.hbtn.io/status
+    Function that fetches https://alx-intranet.hbtn.io/status
     """
-    url = 'https://intranet.hbtn.io/status'
+    url = 'https://alx-intranet.hbtn.io/status'
     r = requests.get(url)
     print('Body response:')
     print('\t- type: {}'.format(type(r.text)))
