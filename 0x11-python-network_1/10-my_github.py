ebec0e25a86fc7a879c7656e891f98f275e9479a
William Osei Prempeh <52207181+NANA-PREMPEH@users.noreply.github.com>
2022-10-22 04:26:35 +0000

    Update 10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,26 +1,21 @@
 #!/usr/bin/python3
-"""
-Module to access to the GitHub API and uses the information
+"""  script that takes your GitHub credentials (username and password) and
+    uses the GitHub API to display your id
 """
 import requests
-from requests.auth import HTTPBasicAuth
 from sys import argv
 
 
-def main(argv):
-    """
-    Script that takes your GitHub credentials (username and password) and
-    uses the GitHub API to display your id.
-    """
-    user = argv[1]
-    password = argv[2]
-    response = requests.get('https://api.github.com/user',
-                            auth=HTTPBasicAuth(user, password))
-    try:
-        profile_info = response.json()
-        print(profile_info['id'])
-    except:
-        print('None')
+def my_github():
+    """sends a POST request to the passed URL with a letter as a parameter"""
+    url = "https://api.github.com/user"
+    values = (argv[1], argv[2])
+    req = requests.get(url, auth=values)
+    if req.status_code >= 400:
+        print("None")
+    else:
+        print(req.json().get('id'))
+
 
 if __name__ == "__main__":
-    main(argv)
+    my_github()
